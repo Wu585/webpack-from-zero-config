5f1e4d6a7136cfc0ec3c87372f1d7268420ff3fa
Wu <15151851516@163.com>
2022-04-16 16:53:23 +0800

    支持 scss 文件

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {x} from './ts-demo.ts'
 import {TsxDemo} from "./tsx-demo.tsx";
 import {abc} from "@/dir1/x";
 import {yyy} from "@/dir1/yyy.ts";
+import '@/scss-demo.scss'
 
 const b = import('./b') // 动态引入，也按需加载
 console.log(abc);
@@ -11,6 +12,7 @@ console.log(x)
 console.log(JsxDemo)
 console.log(TsxDemo)
 console.log(yyy)
+
 const hi = () => {
   console.log('frank')
   console.log(a)
